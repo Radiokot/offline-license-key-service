67319ede7c991feae76ceafaf36f65691266aa9c
Oleg Koretsky <oleg@radiokot.com.ua>
2024-04-11 22:44:28 +0300

    Implement order repo on BTCPay invoices

---
+++ src/main/kotlin/ua/com/radiokot/license/service/orders/BtcPayOrderRepository.kt
@@ -0,0 +1,103 @@
+package ua.com.radiokot.license.service.orders
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.core.io.BigDecimalParser
+import com.fasterxml.jackson.databind.ObjectMapper
+import ua.com.radiokot.license.service.btcpay.greenfield.invocies.GreenfieldStoreInvoicesService
+import ua.com.radiokot.license.service.btcpay.greenfield.invocies.model.GreenfieldInvoice
+import ua.com.radiokot.license.service.btcpay.greenfield.invocies.model.GreenfieldInvoiceCreationData
+import java.math.BigDecimal
+
+typealias OrderUrlFactory = (orderId: String) -> String
+
+class BtcPayOrderRepository(
+    private val orderUrlFactory: OrderUrlFactory,
+    private val speedPolicy: GreenfieldInvoice.SpeedPolicy,
+    private val greenfieldStoreInvoicesService: GreenfieldStoreInvoicesService,
+    private val jsonObjectMapper: ObjectMapper,
+) : OrderRepository {
+    override fun createOrder(
+        id: String,
+        paymentMethodId: String,
+        amount: BigDecimal,
+        currency: String,
+        buyerEmail: String,
+        encodedKey: String
+    ): Order {
+        check(getOrderById(id) == null) {
+            "Order with id '$id' already exists"
+        }
+
+        return greenfieldStoreInvoicesService.createInvoice(
+            GreenfieldInvoiceCreationData(
+                amount = amount.toPlainString(),
+                currency = currency,
+                checkout = GreenfieldInvoice.CheckoutData(
+                    speedPolicy = speedPolicy,
+                    redirectUrl = orderUrlFactory(id),
+                    redirectAutomatically = true,
+                ),
+                metadata = GreenfieldInvoice.Metadata(
+                    orderId = id,
+                    buyerEmail = buyerEmail,
+                    extra = jsonObjectMapper.valueToTree(
+                        MetadataExtra(
+                            paymentMethodId = paymentMethodId,
+                            encodedKey = encodedKey,
+                        )
+                    )
+                )
+            )
+        ).toOrder()
+    }
+
+    override fun getOrderById(orderId: String): Order? =
+        greenfieldStoreInvoicesService.getInvoices(
+            orderId = setOf(orderId),
+        ).firstOrNull()?.toOrder()
+
+    private fun GreenfieldInvoice.toOrder(): Order {
+        val metadata = checkNotNull(this.metadata) {
+            "The invoice must have the metadata"
+        }
+        val metadataExtra: MetadataExtra =
+            jsonObjectMapper.treeToValue(
+                checkNotNull(metadata.extra) {
+                    "The invoice must have the metadata extra"
+                },
+                MetadataExtra::class.java,
+            )
+
+        return Order(
+            id = checkNotNull(metadata.orderId) {
+                "The invoice must have the order ID"
+            },
+            paymentMethodId = metadataExtra.paymentMethodId,
+            amount = BigDecimalParser.parse(this.amount),
+            currency = this.currency,
+            buyerEmail = checkNotNull(metadata.buyerEmail) {
+                "The invoice must have the buyer email"
+            },
+            encodedKey = metadataExtra.encodedKey,
+            status = when (this.status) {
+                GreenfieldInvoice.Status.EXPIRED,
+                GreenfieldInvoice.Status.INVALID ->
+                    Order.Status.CLOSED
+
+                GreenfieldInvoice.Status.NEW,
+                GreenfieldInvoice.Status.PROCESSING ->
+                    Order.Status.PENDING
+
+                GreenfieldInvoice.Status.SETTLED ->
+                    Order.Status.PAID
+            }
+        )
+    }
+
+    private class MetadataExtra(
+        @JsonProperty("paymentMethodId")
+        val paymentMethodId: String,
+        @JsonProperty("encodedKey")
+        val encodedKey: String,
+    )
+}
