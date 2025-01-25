d77ec882c62867122458e1b133b3cbe59da290f5
Oleg Koretsky <oleg@radiokot.com.ua>
2025-01-25 11:07:14 +0200

    Deactivate web store routes

--- src/main/kotlin/ua/com/radiokot/license/service/Application.kt
+++ src/main/kotlin/ua/com/radiokot/license/service/Application.kt
@@ -1,11 +1,10 @@
 package ua.com.radiokot.license.service
 
 import io.javalin.Javalin
-import io.javalin.apibuilder.ApiBuilder.*
+import io.javalin.apibuilder.ApiBuilder.get
+import io.javalin.apibuilder.ApiBuilder.path
 import io.javalin.http.BadRequestResponse
-import io.javalin.http.ForbiddenResponse
 import io.javalin.http.Header
-import io.javalin.http.MethodNotAllowedResponse
 import io.javalin.http.servlet.HttpResponseExceptionMapper
 import io.javalin.json.JavalinJackson
 import io.javalin.rendering.template.JavalinThymeleaf
@@ -23,17 +22,7 @@ import sun.misc.Signal
 import ua.com.radiokot.license.service.api.apiModule
 import ua.com.radiokot.license.service.api.features.FeaturesApiController
 import ua.com.radiokot.license.service.api.issuers.IssuersApiController
-import ua.com.radiokot.license.service.api.issuers.issuance.IssuanceApiController
 import ua.com.radiokot.license.service.api.orders.OrdersApiController
-import ua.com.radiokot.license.service.btcpay.webhook.BtcPayWebhookController
-import ua.com.radiokot.license.service.features.FeaturesController
-import ua.com.radiokot.license.service.features.featuresModule
-import ua.com.radiokot.license.service.orders.OrdersController
-import ua.com.radiokot.license.service.orders.ordersModule
-import ua.com.radiokot.license.service.payment.BtcPayPaymentMethodController
-import ua.com.radiokot.license.service.payment.ManualCheckoutPaymentMethodController
-import ua.com.radiokot.license.service.payment.paymentMethodsModule
-import ua.com.radiokot.license.service.turnstile.cloudflareTurnstileModule
 import ua.com.radiokot.license.service.util.JavalinResponseStatusLogger
 import ua.com.radiokot.license.service.util.KLoggerKoinLogger
 
@@ -54,10 +43,6 @@ object Application : KoinComponent {
 
             modules(
                 apiModule,
-                paymentMethodsModule,
-                ordersModule,
-                featuresModule,
-                cloudflareTurnstileModule,
             )
         }
 
@@ -99,19 +84,6 @@ object Application : KoinComponent {
                 ctx.header(Header.SERVER, "olk-svc")
             }
             .routes {
-                get(
-                    "/",
-                    get<FeaturesController>()::render
-                )
-
-                get(
-                    "buy",
-                    BuyPageController(
-                        featuresRepository = get(),
-                        cloudflareTurnstile = getKoin().getOrNull(),
-                    )::render,
-                )
-
                 path("v1/") {
                     get(
                         "issuers",
@@ -123,11 +95,6 @@ object Application : KoinComponent {
                         get<IssuersApiController>()::getIssuerById
                     )
 
-                    post(
-                        "issuers/{issuerId}/issuance",
-//                        get<IssuanceApiController>()::issueKey
-                    ) { throw ForbiddenResponse() }
-
                     get(
                         "orders/{orderId}",
                         get<OrdersApiController>()::getOrderById
@@ -138,30 +105,6 @@ object Application : KoinComponent {
                         get<FeaturesApiController>()::getFeatures,
                     )
                 }
-
-                path("orders/") {
-                    get(
-                        "{orderId}",
-                        get<OrdersController>()::getOrderById
-                    )
-
-                    post(get<OrdersController>()::createOrder)
-                }
-
-                get(
-                    "checkout/${BtcPayPaymentMethodController.PAYMENT_METHOD_ID}/{orderId}",
-                    get<BtcPayPaymentMethodController>()::checkout,
-                )
-
-                get(
-                    "checkout/${ManualCheckoutPaymentMethodController.PAYMENT_METHOD_ID}/{orderId}",
-                    get<ManualCheckoutPaymentMethodController>()::checkout,
-                )
-
-                post(
-                    "btcpay-event",
-                    get<BtcPayWebhookController>()::handleEvent,
-                )
             }
             .exception(BadRequestResponse::class.java) { e, ctx ->
                 apiLog.debug(e) {
